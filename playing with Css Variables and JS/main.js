b7319872a5c8e0b6a39e31a8b5b7941cb75f4eea
Marian de la Osa <mariandelaosa@gmail.com>
2020-09-04 16:12:58 +0200

    playing with css and JS

---
+++ playing with Css Variables and JS/main.js
@@ -0,0 +1,12 @@
+"use script"
+
+const inputs = document.querySelectorAll(".controls input");
+
+function handleChanges() {
+console.log(this.value); 
+const suffix = this.dataset.sizing || '';
+document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleChanges));
+inputs.forEach(input => input.addEventListener('mousemove', handleChanges));
