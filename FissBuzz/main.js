22795dd8c2e17b432fa970f02238c964eba0dc66
Marian de la Osa <mariandelaosa@gmail.com>
2020-09-08 17:17:45 +0200

    change i=0 and html

--- FissBuzz/main.js
+++ FissBuzz/main.js
@@ -3,7 +3,7 @@
 // si un número es múltiplo de 5 tiene que decir buzz
 // siun número es múltiplo de 3 y de 5, tiene que decir FizzBuzz
 
-for (let i = 1; i <= 100; i++){
+for (let i = 0; i <= 100; i++){
   if (i % 3 === 0 && i % 5 === 0){
     console.log(`${i} fissbuzz`);
   }else if (i%3 === 0){
